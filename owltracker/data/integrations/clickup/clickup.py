677ac619c6aab40f73e263fb0396ce8d583072af
Felipe Malbergier <f.malbergier@gmail.com>
2022-09-10 15:31:15 +0300

    clickup revamped

--- owltracker/data/integrations/clickup/clickup.py
+++ owltracker/data/integrations/clickup/clickup.py
@@ -1,55 +1,61 @@
 import requests
-import json
+import yaml
 import time
 
 
 with open("config.json") as f:
-    config = json.load(f)
+    config = yaml.safe_load(f)
 
-BASE_URL = "https://api.clickup.com/api/v2/"
-LIST_IDS = [180856365, # freelancing
-        180856325, # projects
-        180856407] # personal
-WORKSPACE_ID = 31075257
-TASK_INFORMATION = ['id', 'name', 'team_id', 'list']
-headers = {
-    'Content-Type': 'application/json',
-    'Authorization': config['api_token']
-}
+class Clickup:
+    BASE_URL = "https://api.clickup.com/api/v2/"
+    WORKSPACE_ID = config['workspace_id']
+    TASK_INFORMATION = ['id', 'name', 'team_id', 'list']
 
+    def __init__(self) -> None:
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Authorization': config['api_token']
+        }
 
+    @staticmethod
+    def request_api(method, url, headers, params=None, payload=None):
+        response = requests.request(method, url, headers=headers, params=params, json=payload)
+        return response
+        
 
-def get_list_tasks() -> list[dict]:
-    # TODO Implement grequests
-    payload={}
-    tasks = list()
-    for list_id in LIST_IDS:
-        url = BASE_URL + "list/{list_id}/task".format(list_id=list_id)
-        response = requests.request("GET", url, headers=headers, data=payload)
+    def get_list_tasks(self) -> list[dict]:
+        # TODO Implement grequests
+        params = {"statuses[0]": 'todo',
+                "statuses[1]": 'doing'}
+        url = self.BASE_URL + f"/team/{self.WORKSPACE_ID}/task?"
+        response = self.request_api("GET", url, self.headers, params=params)
+        
+        tasks = list()
         for task in response.json().get('tasks', {}):
-            task_to_return = {info: task[info] for info in TASK_INFORMATION}
+            task_to_return = {info: task[info] for info in self.TASK_INFORMATION}
             task_to_return.update({'integration': "clickup"})
             tasks.append(task_to_return)
-    return tasks
-    
+        return tasks
+        
 
-def update_time_task(task_id: str, task_time: float) -> requests.models.Response:
-    url = f"https://api.clickup.com/api/v2/team/{WORKSPACE_ID}/time_entries/"
-    payload={
-    "description": "from owltracker",
-    "tags": [
-        {
-        "name": "owltracker",
-        "tag_bg": "#BF55EC",
-        "tag_fg": "#FFFFFF"
+    def update_time_task(self, task_id: str, task_time: float) -> requests.models.Response:
+        url = f"https://api.clickup.com/api/v2/team/{self.WORKSPACE_ID}/time_entries/"
+        payload={
+        "description": "from owltracker",
+        "tags": [
+            {
+            "name": "owltracker",
+            "tag_bg": "#BF55EC",
+            "tag_fg": "#FFFFFF"
+            }
+        ],
+        "start": int(time.time() - task_time) * 1000,
+        "duration": int(task_time * 1000),
+        "tid": f"{task_id}"
         }
-    ],
-    "start": int(time.time() - task_time) * 1000,
-    "duration": int(task_time * 1000),
-    "tid": f"{task_id}"
-    }
-    response = requests.request("POST", url, headers=headers, json=payload)
-    return response
+        response = self.request_api("POST", url, headers=self.headers, payload=payload)
+        return response
 
 if __name__ == "__main__":
-    get_list_tasks()
+    clickup = Clickup()
+    tasks = clickup.get_list_tasks()
