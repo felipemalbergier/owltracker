c130087ceb10ecda1c7974f9ea581938a62a880d
Felipe <f.malbergier@gmail.com>
2023-04-18 17:09:21 +0300

    get name window windows

---
+++ owltracker/data/activity_tracker/activity_tracker_windows.py
@@ -0,0 +1,13 @@
+import win32gui, win32process, psutil
+
+def get_active_window_info_windows() -> dict:
+    try:
+        foreground_window = win32gui.GetForegroundWindow()
+        pid = win32process.GetWindowThreadProcessId(foreground_window)
+        process_name = psutil.Process(pid[-1]).name()
+        window_title = win32gui.GetWindowText(foreground_window)
+        return {"process_name": process_name, "window_title": window_title}
+    except:
+        # TODO add logger
+        pass
+
