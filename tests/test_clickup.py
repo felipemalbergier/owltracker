086ee25d8b3f456ad3d56bbefa0cf04298bac3b0
Felipe <f.malbergier@gmail.com>
2023-12-11 17:37:16 -0300

    fix test_clickup

--- tests/test_clickup.py
+++ tests/test_clickup.py
@@ -1,20 +1,22 @@
 import unittest
-from unittest.mock import patch
-from owltracker.data.integrations.clickup import Clickup
+from unittest.mock import Mock, patch
+
+# from owltracker.data.integrations.clickup.clickup import Clickup        
+from owltracker.data.integrations.clickup.clickup import Clickup
 
 class TestClickup(unittest.TestCase):
-    def setUp(self):
-        self.clickup = Clickup()
 
-    @patch('requests.get')
-    def test_get_list_tasks(self, mock_get):
-        mock_get.return_value.ok = True
-        mock_get.return_value.json.return_value = {"tasks": [{"id": 1, "name": "Test Task"}]}
+    @patch('owltracker.data.integrations.clickup.clickup.requests.request')
+    def test_get_list_tasks(self, mock_request):
+        mock_request.return_value.ok = True
+        mock_request.return_value.json.return_value = {"tasks": [{"id": 1, "name": "Test Task"}]}
         
-        tasks = self.clickup.get_list_tasks()
+        clickup = Clickup()
+        tasks = clickup.get_list_tasks()
         self.assertEqual(len(tasks), 1)
         self.assertEqual(tasks[0].id, 1)
         self.assertEqual(tasks[0].title, "Test Task")
+
 
 if __name__ == '__main__':
     unittest.main()
